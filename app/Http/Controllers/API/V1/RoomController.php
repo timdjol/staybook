7b590514434983abc8f29e05bef4e337d032f799
timdjol <02_07_92@mail.ru>
2025-03-04 17:06:55 +0600

    generate doc

--- app/Http/Controllers/API/V1/RoomController.php
+++ app/Http/Controllers/API/V1/RoomController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Room;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 
 class RoomController extends Controller
@@ -30,18 +33,31 @@ class RoomController extends Controller
         return $room;
     }
 
+    /**
+     * @param Request $request
+     * @return Application|ResponseFactory|\Illuminate\Foundation\Application|Response
+     */
     public function store(Request $request)
     {
         Room::create($request->all());
         return response('Room created successfully.', 201);
     }
 
+    /**
+     * @param Request $request
+     * @param Room $room
+     * @return Room
+     */
     public function update(Request $request, Room $room)
     {
         $room->update($request->all());
         return $room;
     }
 
+    /**
+     * @param Room $room
+     * @return Application|ResponseFactory|\Illuminate\Foundation\Application|Response
+     */
     public function destroy(Room $room)
     {
         $room->delete();
